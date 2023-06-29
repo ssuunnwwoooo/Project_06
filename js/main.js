c352b0b330fa43aa9eeb37de39c9e08ccc41863b
sunwoo <133613205+ssuunnwwoooo@users.noreply.github.com>
2023-06-29 16:23:13 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -38,26 +38,17 @@ $(function () {
         mainSlide.slideNext();
     });
 
-    $(document).ready(function () {
-
-        $('ul.tabs li').click(function () {
-            var tab_id = $(this).attr('data-tab');
-
-            $('ul.tabs li').removeClass('current');
-            $('.tab-content').removeClass('current');
-
-            $(this).addClass('current');
-            $("#" + tab_id).addClass('current');
-        })
-    });
-
-
     $('.content_slide').slick({
         infinite: true,
         slidesToShow: 3,
         slidesToScroll: 1,
         arrows: false,
         setPosition: 0,
+        autoplay: {
+            delay: 5000,
+            disableOnInteraction: false,
+        },
+
     });
     $('.event_slide').slick({
         infinite: true,
@@ -67,16 +58,12 @@ $(function () {
         setPosition: 0,
     });
 
-    function openModal(modalname) {
-        $('.content_slide , .event_slide').resize()
-        $('.content_slide , .event_slide').slick('refresh');
-    };
 
     $('.main_content .arrows .left').on('click', function () {
-        $('.content_slide , .event_slide').slick('slickPrev')
+        $('.content_slide').slick('slickPrev')
     });
     $('.main_content .arrows .right').on('click', function () {
-        $('.content_slide , .event_slide').slick('slickNext')
+        $('.content_slide').slick('slickNext')
     });
 
 

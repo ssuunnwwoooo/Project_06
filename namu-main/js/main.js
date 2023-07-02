70fd2759ec1ef43eeddd6513d3b8adf3609b7bbf
sunwoo <133613205+ssuunnwwoooo@users.noreply.github.com>
2023-07-02 19:56:24 +0900

    Add files via upload

---
+++ namu-main/js/main.js
@@ -0,0 +1,62 @@
+$(function () {
+
+    $(window).on('scroll', function () {
+        let sct = $(window).scrollTop();
+        sct > 0
+            ? $('.header').addClass('on')
+            : $('.header').removeClass('on');
+    });
+
+
+    $(window).on('scroll', function () {
+        let sct = $(window).scrollTop();
+        $('._se_').each(function () {
+            if (sct + $(window).innerHeight() > $(this).offset().top) {
+                $(this).addClass('on')
+            } else {
+                $(this).removeClass('on')
+            }
+        })
+    });
+
+
+    const mainSlide = new Swiper('.main_slide', {
+        loop: true,
+        parallax: true,
+        speed: 1000,
+        autoplay: {
+            delay: 4000,
+            disableOnInteraction: false,
+        },
+        slideActiveClass: 'on',
+    });
+
+    $('.main_visual .arrows .left').on('click', function () {
+        mainSlide.slidePrev();
+    });
+    $('.main_visual .arrows .right').on('click', function () {
+        mainSlide.slideNext();
+    });
+
+    $('.content_slide').slick({
+
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false,
+
+
+    });
+
+
+    $('.main_content .arrows .left').on('click', function () {
+        $('.content_slide').slick('slickPrev')
+    });
+    $('.main_content .arrows .right').on('click', function () {
+        $('.content_slide').slick('slickNext')
+    });
+
+
+
+})
